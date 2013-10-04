603916a87f52f942aa2fd140e8e8018e09c6f2f7
Joona Somerkivi <joona@somerkivi.net>
2013-10-04 23:06:03 +0300

    Added JSomerstoneDaysWithoutBundle & updated composer.json/lock

--- app/autoload.php
+++ app/autoload.php
@@ -5,6 +5,7 @@ use Doctrine\Common\Annotations\AnnotationRegistry;
 $loader = require __DIR__.'/../vendor/autoload.php';
 $loader->add('SclWhois', __DIR__.'/../vendor/sclinternet/scl-whois/src');
 $loader->add('SclSocket', __DIR__.'/../vendor/sclinternet/scl-socket/src');
+$loader->add('JSomerstone', __DIR__.'/../vendor/jsomerstone/dayswithout/src');
 
 // intl
 if (!function_exists('intl_get_error_code')) {
